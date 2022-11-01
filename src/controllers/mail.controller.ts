6b2ebb1d93c8f742ac4f87421ace7c917c683d0a
Asen Yovchev <ayovcheff@gmail.com>
2022-11-01 17:13:14 +0200

    integrate email model and validator

--- src/controllers/mail.controller.ts
+++ src/controllers/mail.controller.ts
@@ -1,5 +1,19 @@
 import { Request, Response } from 'express';
+import { Email } from '../models/email.model';
+import { EmailValidator } from '../services/email.validator';
 
 export const create = async (req: Request, res: Response): Promise<Response> => {
-  return res.sendStatus(200);
+  const emailData = req.body;
+  const feedback =  await new EmailValidator(emailData).validate();
+
+  if(feedback.length > 0) {
+    return res.send(400).json(feedback);
+  }
+
+  try{
+    const result = await Email.create(emailData);
+    return res.status(200).send({data: result});
+  } catch(e) {
+    return res.status(500).send(e);
+  }
 };
