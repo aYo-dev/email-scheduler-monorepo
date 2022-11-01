479cb61d4be7270a3b8bed0d05d993b14ae7a98e
Asen Yovchev <ayovcheff@gmail.com>
2022-11-01 17:06:39 +0200

    refactoring

--- src/controllers/mail.controller.ts
+++ src/controllers/mail.controller.ts
@@ -3,7 +3,3 @@ import { Request, Response } from 'express';
 export const create = async (req: Request, res: Response): Promise<Response> => {
   return res.sendStatus(200);
 };
-
-export const readAll = async (req: Request, res: Response): Promise<Response> => {
-  return res.sendStatus(200);
-};
