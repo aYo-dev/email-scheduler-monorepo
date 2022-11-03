0103759cc5f6d062fa53885f41efd0ad81dd3d2e
Asen Yovchev <ayovcheff@gmail.com>
2022-11-03 14:24:01 +0200

    create the layout of the form

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import './App.css';
+import {LocalizationProvider} from '@mui/x-date-pickers';
 import { EmailScheduleForm } from './components/EmailScheduleForm';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <EmailScheduleForm />
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Header />
+        <EmailScheduleForm />
+      </LocalizationProvider>
     </div>
   );
 }
--- src/components/EmailScheduleForm.tsx
+++ src/components/EmailScheduleForm.tsx
@@ -1,18 +1,62 @@
-import { Typography } from "@mui/material";
-import { useState } from "react";
+import { Box, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, Stack, TextField, Typography } from "@mui/material";
+import { equals } from "ramda";
+import { useMemo, useState } from "react";
+import { useApi } from "../hooks/useApi";
+import EndSelect from "./EndOptions";
 import Repeat from "./Repeat";
+
+type EndOptions = 'never' | 'on' | 'after';
 
 export const EmailScheduleForm = () => {
   const [selectedDays, selectDays] = useState(['']);
+  const [end, setEnd] = useState('never');
+  const [receiver, setReceiver] = useState('');
+  const [content, setContent] = useState('');
+  const [sendingType, setSendingType] = useState('once');
 
   const handleDayChange = (selectedDays: string[]) => {
     selectDays(selectedDays);
   };
 
+  const isRecurrently = useMemo(() => equals(sendingType, 'recurrently'), [sendingType]);
+
   return (
-    <> 
-      <Typography>Repeat on:</Typography>
-      <Repeat handleChange={handleDayChange} values={selectedDays}/>
+    <>
+      <Stack spacing={2} padding={2} sx={{maxWidth: '400px'}}>
+        <TextField
+          label="Receiver"
+          value={receiver}
+          onChange={(e) => setReceiver(e.target.value)}
+          variant="outlined"
+          sx={{width: '100%'}}
+          type="email"
+        />
+        <TextField
+          label="Email content"
+          onChange={(e) => setContent(e.target.value)}
+          multiline
+          rows={6}
+        />
+      </Stack>
+      <Box>
+        <FormControl>
+          <FormLabel id="demo-radio-buttons-group-label">Send it</FormLabel>
+          <RadioGroup
+            row
+            defaultValue="once"
+            name="send-it"
+            onChange={(v) => setSendingType(v.target.value)}
+          >
+            <FormControlLabel value="once" control={<Radio />} label="Once" />
+            <FormControlLabel value="recurrently" control={<Radio />} label="Recurrently" />
+          </RadioGroup>
+        </FormControl>
+      </Box>
+      {isRecurrently && <Box>
+        <Typography>Repeat on:</Typography>
+        <Repeat handleChange={handleDayChange} values={selectedDays}/>
+        <EndSelect onEndTypeChange={setEnd}/>
+      </Box>}
     </>
   );
 }
