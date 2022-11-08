41b332900f63090531d1cf8acf4de6647d22f371
Asen Yovchev <ayovcheff@gmail.com>
2022-11-08 23:16:49 +0200

    write proper documentation

--- src/controllers/mail.controller.ts
+++ src/controllers/mail.controller.ts
@@ -1,6 +1,45 @@
 import { Request, Response } from 'express';
+import { equals, ifElse } from 'ramda';
+import { EndTypes, SendingTypes } from '../enums';
 import { Email } from '../models/email.model';
 import { EmailValidator } from '../services/email.validator';
+import { createCron, calculateEndDate } from '../util';
+
+/**
+ * @param endType 
+ * @param sendingType 
+ * @returns boolean
+ */
+const shouldCalculateEndDate = (endType: EndTypes, sendingType: SendingTypes) =>
+  equals(endType, EndTypes.after) && equals(sendingType, SendingTypes.recurrently);
+
+/**
+ * decides if is necessary to create cron
+ * @param sendingType 
+ * @param selectedDays 
+ * @param repeatAt 
+ * @returns return empty string or cron string
+ */
+const getInterval = (sendingType: SendingTypes, selectedDays: string[], repeatAt: string) =>
+  ifElse(
+    equals(SendingTypes.recurrently),
+    () => createCron(selectedDays, repeatAt),
+    () => '',
+  )(sendingType);
+
+/**
+ * Calculate end date if sendting type is `recurrently` and edning type is `after` (i.e. campaign will end after n occurrences)
+ * otherwise return default date
+ * @param defaultDate 
+ * @param interval 
+ * @param occurrences 
+ * @returns 
+ */
+const getEndDate = (defaultDate: string, interval: string, occurrences: number) => ifElse(
+  shouldCalculateEndDate,
+  () => calculateEndDate(interval, occurrences),
+  () => defaultDate,
+);
 
 export const create = async (req: Request, res: Response): Promise<Response> => {
   const requestData = req.body;
@@ -9,6 +48,14 @@ export const create = async (req: Request, res: Response): Promise<Response> => 
   if(feedback.length > 0) {
     return res.status(400).send({data: feedback});
   }
+  
+  const interval = getInterval(
+    requestData.sendingType,
+    requestData.selectedDays,
+    requestData.repeatAt
+  );
+
+  const getEndDateCb = getEndDate(requestData.endDate, requestData.interval, requestData.occurrances)
 
   const emailData = {
     receiver: requestData.receiver,
@@ -16,8 +63,9 @@ export const create = async (req: Request, res: Response): Promise<Response> => 
     sendingType: requestData.sendingType,
     sendingTypeOptions: {
       when: requestData.when,
-      end: requestData.end,
+      end: getEndDateCb(requestData.endType ,requestData.sendingType),
       occurences: requestData.occurences,
+      interval,
     },
   };
 
